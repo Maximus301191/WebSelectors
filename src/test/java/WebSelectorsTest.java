75899e6b132bdc66bd2951a3a2cf262ea703c559
Maximus301191 <maxalut301191@gmail.com>
2025-04-27 15:28:47 +0300

    Positive commit

---
+++ src/test/java/WebSelectorsTest.java
@@ -0,0 +1,50 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+import static java.awt.SystemColor.text;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class WebSelectorsTest {
+        private WebDriver driver;
+
+        @BeforeAll
+        static void setUpAll() {
+            WebDriverManager.chromedriver().setup();
+        }
+
+        @BeforeEach
+        void setUp() {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--headless");
+            driver = new ChromeDriver(options);
+            driver.get("http://localhost:9999");
+        }
+
+        @AfterEach
+        void tearDown() {
+            driver.quit();
+            driver = null;
+        }
+
+    @Test
+    void shouldSendFormCorrect() {
+        WebElement form = driver.findElement(By.cssSelector("form"));
+        form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иван Петров");
+        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79816589633");
+        form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        form.findElement(By.cssSelector("button")).click();
+        WebElement resultElement = driver.findElement(By.cssSelector("[data-test-id=o   rder-success]"));
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", resultElement.getText().trim());
+    }
+}
